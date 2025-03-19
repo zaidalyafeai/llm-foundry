ede4c1b631dd2af4676a23dcf3e3776ae5a3ca3c
Zaid Alyafeai <alyafey22@gmail.com>
2025-03-19 19:40:18 +0300

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,8 @@
+composer train/train.py \
+  train/yamls/pretrain/mpt-125m.yaml \
+  variables.data_local=my-copy-c4 \
+  train_loader.dataset.split=train_small \
+  eval_loader.dataset.split=val_small \
+  max_duration=10ba \
+  eval_interval=0 \
+  save_folder=mpt-125m
