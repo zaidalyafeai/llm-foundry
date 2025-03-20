ecd390ee0fe73b6240a9c0c0a3d32bdf01ce937c
Zaid Alyafeai <alyafey22@gmail.com>
2025-03-20 11:54:53 +0300

    Update train.sh

--- train.sh
+++ train.sh
@@ -5,5 +5,5 @@ composer train/train.py \
   train_loader.dataset.split=train_small \
   eval_loader.dataset.split=val_small \
   max_duration=100ba \
-  eval_interval=0 \
+  eval_interval=50 \
   save_folder=mpt-125m
