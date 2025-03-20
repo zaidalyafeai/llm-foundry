e6e014d6fbfd475087369f19f39d5dbfcc7485b0
Zaid Alyafeai <alyafey22@gmail.com>
2025-03-20 12:13:56 +0300

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 cd scripts
 composer train/train.py \
-  train/yamls/pretrain/mpt-125m.yaml \
+  train/yamls/pretrain/mpt-1b.yaml \
   variables.data_local=my-copy-c4 \
   train_loader.dataset.split=train_small \
   eval_loader.dataset.split=val_small \
   max_duration=100ba \
   eval_interval=50 \
-  save_folder=mpt-125m
+  save_folder=mpt-1b
